e0059d1d01abd6e304c2f964774e1e9335e6b95f
ADIL <adilshaik9112@gmail.com>
2026-09-25 18:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    this.listeners.get(event).add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    if (!this.listeners.has(event)) return;
+    this.listeners.get(event).delete(callback);
+  }
+
+  dispatchEvent(event) {
+    if (!this.listeners.has(event)) return;
+    for (const callback of this.listeners.get(event)) {
+      callback();
+    }
+  }
+}
